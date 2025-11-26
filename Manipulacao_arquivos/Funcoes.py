6b71f69630b77ed81a7b1213176cf354d26e573e
Kevin Ricardino <kevin.j.bernardes@gmail.com>
2025-11-26 09:28:25 -0300

    Arquivos e Criptografia

---
+++ Manipulacao_arquivos/Funcoes.py
@@ -0,0 +1,25 @@
+def perguntar():
+    margem = "=" * 32
+    return input(
+        f"\n{margem}\n"
+        "<I> - Para Inserir um usuário\n"
+        "<P> - Para Pesquisar um usuário\n"
+        "<E> - Para Excluir um usuário\n"
+        "<L> - Para Listar um usuário\n"
+        f"{margem}\n"
+        "\nInsira o que deseja realizar: "
+    ).upper()
+
+def inserir(dicionario):
+    dicionario[input("Digite o login: ").upper()] = [
+        input("Digite o nome: ").upper(),
+        input("Digite a última data de acesso: "),
+        input("Qual a última estação acessada: ").upper()
+    ]
+
+    salvar(dicionario)
+
+def salvar(dicionario):
+    with open("bd.txt", "a") as arquivo:
+        for chave, valor in dicionario.items():
+            arquivo.write(chave + ":" + str(valor))
